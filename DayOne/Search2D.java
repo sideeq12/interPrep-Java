e10ce1a4ddbbaad5b71f447bde944d487684ac10
sideeq12 <abdwaheed2018@gmail.com>
2023-07-07 11:58:50 +0100

    2d search array

--- DayOne/Search2D.java
+++ DayOne/Search2D.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+
 class Search2D {
 
     public static void main(String args[]){
-        
+        int[][] myArr = {
+            {2,3,4}, 
+            {-1,4,6,2},
+            {0,3,-2,7,4,-1}
+        };
+        int[] result = search(myArr, 2);
+    System.out.println(Arrays.toString(result));
+    }
+
+    static int[] search(int[][] list, int target){
+        for(int row = 0; row <  list.length; row++){
+            for(int col = 0; col < list[row].length; col++){
+                if(list[row][col] ==  target ){
+                    return new int[]{row, col };
+                }
+            }
+        }
+        return new int[]{-1 , -1};
     }
 }
