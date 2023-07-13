0799d2eeb94cf8d0b8bd65f4c44cc1f565ec74a0
sideeq12 <abdwaheed2018@gmail.com>
2023-07-13 11:33:29 +0100

    OA binsearch

--- DayFour/orderAgnostic.java
+++ DayFour/orderAgnostic.java
@@ -1,7 +1,7 @@
 class orderAgnostic{
     public static void main(String args[]){
         int[] arr = {2,3,5,7,8,9,12,55,78 };
-        int target = 3;
+        int target = 12;
         int ans = search(arr, target);
         System.out.println(ans);
     }
@@ -11,7 +11,10 @@ class orderAgnostic{
         // beginning of array
         int start = 0;
         int end = arr.length -1;
+
+        // confirming if its in Asc or Descending order
         boolean isAsc = arr[start] < arr[end];
+
          while(start <= end ){
             // take the middle
             int middle =  start + (end-start)/2;
@@ -20,9 +23,7 @@ class orderAgnostic{
             }
             if(isAsc){
                 if(arr[middle ] > target){
-
                 end = middle -1;
-
             }else {
                 start = middle +1;
             }
